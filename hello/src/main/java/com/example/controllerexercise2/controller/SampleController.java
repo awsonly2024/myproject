9599ffd46571c06c61bbfef8f9efb62d13cd3e36
khj20231204 <natista00@naver.com>
2024-11-06 10:13:59 +0900

    aa

--- hello/src/main/java/com/example/controllerexercise2/controller/SampleController.java
+++ hello/src/main/java/com/example/controllerexercise2/controller/SampleController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 	@RequestMapping("/")
 	public String hello() {
+		dd
 		return "Hello Spring Boot";
 	}
 }
