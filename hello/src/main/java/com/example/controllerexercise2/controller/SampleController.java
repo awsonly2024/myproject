c3df18a3ec62ebf75f6ab54dcdcc38891d57ddd0
khj20231204 <natista00@naver.com>
2024-11-06 10:11:37 +0900

    first mit

---
+++ hello/src/main/java/com/example/controllerexercise2/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.example.controllerexercise2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+}
---
+++ hello/src/test/java/com/example/controllerexercise2/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controllerexercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
