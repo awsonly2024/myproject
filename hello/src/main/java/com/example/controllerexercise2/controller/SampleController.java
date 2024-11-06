563d20c881fb9ed8ea315a1733e5ac6b3f816d9c
khj20231204 <natista00@naver.com>
2024-11-06 10:43:45 +0900

    user2 branch NewFile.xlm

--- hello/src/main/java/com/example/controllerexercise2/controller/SampleController.java
+++ hello/src/main/java/com/example/controllerexercise2/controller/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 	@RequestMapping("/")
 	public String hello() {
-		dd
+		"dfdsf";
 		return "Hello Spring Boot";
 	}
 }
